55adc5f5b4e183307df0df764d23968a2e07e4b9
Bakdaulet Berdikul <bbbakdaulet1995@gmail.com>
2024-04-09 11:03:53 +0500

    Initial Commit

---
+++ MTracker/MTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTrackerApp.swift
+//  MTracker
+//
+//  Created by Bakdaulet on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
